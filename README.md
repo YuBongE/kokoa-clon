5c0e5d85773ab676e2eb09515922725f2103c7ac
YuBongE <133131969+YuBongE@users.noreply.github.com>
2023-05-11 13:58:20 +0000

    1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kokoa Clone 2023 by YuBong
+
+나는 성공할 것이다.
--- study/23.05.10.js
+++
@@ -1,9 +0,0 @@
-- Media query는 조건을 추가 할 수 있는 방법. ex) 스크린이 가로일 때 혹은 조건 참이라면 내가 적은 css 코드를 실행함. media 코드는 and를 사용해서 연결 됨
-  
-- @media screen and (max-width: 00px) {} 을 이용하여 몇 픽셀부터는 달라보이도록 만들 수 있다. 이를 통해 스크린의 사이즈를 알 수 있다.
-- min 사이즈와 max사이즈를 조절하여 단계별로 만들면, 스크린 사이즈의 범위를 알 수 있다.
-- 브라우저에서 inspect의 device toolbar를 이용하여 핸드폰 기종 별 사이즈로 브라우저를 볼 수 있다.
-- media screen에 (orientation: landscape)를 이용하면, 세로모드인지 가로모드인지도 구별 할 수 있다.
-
-
-  
